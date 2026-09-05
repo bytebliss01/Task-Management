29ad93eceba9978cad52da974f3d55580b4c8f8e
bytebliss01 <bytebliss01@github.com>
2026-09-05 22:50:08 +0530

    Add dark mode toggle

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AuthContextProvider from "./context/AuthContext.jsx";
+
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <App />
   </StrictMode>
 );
